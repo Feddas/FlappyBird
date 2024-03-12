a5b4fa71b60c9a5a64389a16f00ec30df5e4e766
Mariana Rios Silveira Carvalho <marianariossc@gmail.com>
2024-03-12 16:13:57 -0400

    feat: Add score system.

---
+++ Assets/Scripts/PipeIncreaseScore.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeIncreaseScore : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            Score.instance.UpdateScore();
+        }
+    }
+}
