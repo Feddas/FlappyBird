a78ec0aac1efcaa2c0476a8e8cd21554320e8e61
Feddas <feddas00@gmail.com>
2025-06-25 19:51:39 -0700

    bug: Ground moves from players perspective. Sasquatch left this bit out of the youtube tutorial. feat: UI redone for better responsive design support

--- Assets/Scripts/LoopGround.cs
+++ Assets/Scripts/LoopGround.cs
@@ -4,25 +4,30 @@ using UnityEngine;
 
 public class LoopGround : MonoBehaviour
 {
-    [SerializeField] private float _speed = 1f;
+    [SerializeField] private float _speed = 0.65f;
     [SerializeField] private float _width = 6f;
 
     private SpriteRenderer _spriteRenderer;
     private Vector2 _startSize;
+    private Vector3 _startPosition;
 
     private void Start()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _startSize = new Vector2(_spriteRenderer.size.x, _spriteRenderer.size.y);
+        _startPosition = transform.position;
     }
 
     private void Update()
     {
+        transform.position += Vector3.left * _speed * Time.deltaTime;
+
         _spriteRenderer.size = new Vector2(_spriteRenderer.size.x + _speed * Time.deltaTime, _spriteRenderer.size.y);
 
         if (_spriteRenderer.size.x > _width)
         {
             _spriteRenderer.size = _startSize;
+            transform.position = _startPosition;
         }
     }
 }
