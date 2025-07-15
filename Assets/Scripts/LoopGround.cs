5a8a9fbd9fadf01584f7bf351013f3e72fdec1b9
Feddas <feddas00@gmail.com>
2025-07-14 17:46:10 -0700

    bug: ground box collider matches sprite size

--- Assets/Scripts/LoopGround.cs
+++ Assets/Scripts/LoopGround.cs
@@ -8,25 +8,32 @@ public class LoopGround : MonoBehaviour
     [SerializeField] private float _width = 6f;
 
     private SpriteRenderer _spriteRenderer;
+    private BoxCollider2D _collider2d;
     private Vector2 _startSize;
     private Vector3 _startPosition;
 
     private void Start()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _collider2d = GetComponent<BoxCollider2D>();
         _startSize = new Vector2(_spriteRenderer.size.x, _spriteRenderer.size.y);
         _startPosition = transform.position;
     }
 
     private void Update()
     {
-        transform.position += Vector3.left * _speed * Time.deltaTime;
+        // "/ 2" accounts for pivot being in center, size increasing both left and right side
+        transform.position += Vector3.left * _speed / 2 * Time.deltaTime;
 
-        _spriteRenderer.size = new Vector2(_spriteRenderer.size.x + _speed * Time.deltaTime, _spriteRenderer.size.y);
+        _collider2d.size
+            = _spriteRenderer.size
+            = new Vector2(_spriteRenderer.size.x + _speed * Time.deltaTime, _spriteRenderer.size.y);
 
         if (_spriteRenderer.size.x > _width)
         {
-            _spriteRenderer.size = _startSize;
+            _collider2d.size
+                = _spriteRenderer.size
+                = _startSize;
             transform.position = _startPosition;
         }
     }
